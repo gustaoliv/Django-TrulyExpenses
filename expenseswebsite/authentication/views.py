5da491ecd4c91bec73238059b8d868f282908d99
gustaoliv <gusta.oliv4@gmail.com>
2022-03-09 19:45:36 -0300

    add alertas erro

--- expenseswebsite/authentication/views.py
+++ expenseswebsite/authentication/views.py
@@ -4,6 +4,7 @@ import json
 from django.http import JsonResponse
 from django.contrib.auth.models import User
 from validate_email import validate_email
+from django.contrib import messages
 # Create your views here.
 
 
@@ -38,4 +39,12 @@ class EmailValidationView(View):
 
 class RegistrationView(View):
     def get(self, request):
+        return render(request, 'authentication/register.html')
+
+
+    def post(self, request):
+        messages.success(request, 'Registration Sucess')
+        # messages.warning(request, 'Registration Warning')
+        # messages.info(request, 'Registration Info')
+        # messages.error(request, 'Registration Error')
         return render(request, 'authentication/register.html')
--- expenseswebsite/expenseswebsite/settings.py
+++ expenseswebsite/expenseswebsite/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from decouple import config
 import os
 from dj_database_url import parse as dburl
+from django.contrib import messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,3 +135,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
