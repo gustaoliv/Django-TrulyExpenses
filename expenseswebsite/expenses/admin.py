4cc0e7ce797a1ac9ba1802692920ca58f3711c0a
gustaoliv <gusta.oliv4@gmail.com>
2022-03-16 11:55:02 -0300

    custom admin page

--- expenseswebsite/expenses/admin.py
+++ expenseswebsite/expenses/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from .models import Expense, Category
 
 
-admin.site.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('amount', 'description', 'owner', 'category', 'date')
+    search_field = ('description', 'category', 'date')
+    list_per_page = 5
+
+
+
+admin.site.register(Expense, ExpenseAdmin)
 admin.site.register(Category)
